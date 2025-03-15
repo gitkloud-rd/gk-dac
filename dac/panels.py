bc795b7529108231360a68308fcd1785c1827cfb
Bharath Pantala <bpantala@gitkloud.com>
2025-03-15 08:55:36 +0530

    added

---
+++ dac/panels.py
@@ -0,0 +1,57 @@
+
+class PanelLibrary:
+    def __init__(self):
+        pass
+        # self.grafana_url = grafana_url
+        # self.api_key = api_key
+
+    def cpu_panel(self) -> dict:
+        cpu_panel = {
+                    "type": "graph",
+                    "title": "CPU Usage",
+                    "datasource": "Prometheus",
+
+                    "targets": [
+                        {
+                            "expr": "node_cpu_seconds_total{namespace='monitoring'}",
+                            "legendFormat": "{{pod}}",
+                            "refId": "A"
+                        }
+                    ],
+                    "gridPos": {"x": 0, "y": 0, "w": 6, "h": 6}
+                }
+        return cpu_panel
+
+    def mem_panel(self) -> dict:
+        mem_panel = {
+                    "type": "graph",
+                    "title": "Mem Usage",
+                    "datasource": "Prometheus",
+
+                    "targets": [
+                        {
+                            "expr": "node_cpu_seconds_total{namespace='monitoring'}",
+                            "legendFormat": "{{pod}}",
+                            "refId": "A"
+                        }
+                    ],
+                    "gridPos": {"x": 6, "y": 0, "w": 6, "h": 6}
+                }
+        return mem_panel
+
+    def net_panel(self) -> dict:
+        net_panel = {
+                    "type": "stat",
+                    "title": "CPU Usage",
+                    "datasource": "Prometheus",
+
+                    "targets": [
+                        {
+                            "expr": "node_cpu_seconds_total{namespace='monitoring'}",
+                            "legendFormat": "{{pod}}",
+                            "refId": "A"
+                        }
+                    ],
+                    "gridPos": {"x": 12, "y": 6, "w": 6, "h": 6}
+                }
+        return net_panel
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dac.grafana import Connect
+from dac.panels import PanelLibrary as pl
 from grafana_client import GrafanaApi, TokenAuth
 import os
 
@@ -20,58 +21,57 @@ grafana = GrafanaApi.from_url(
 
 print(grafana.health.check())
 
-cpu_panel = {
-        "type": "graph",
-        "title": "CPU Usage",
-        "datasource": "Prometheus",
+# cpu_panel = {
+#         "type": "graph",
+#         "title": "CPU Usage",
+#         "datasource": "Prometheus",
 
-        "targets": [
-            {
-                "expr": "node_cpu_seconds_total{namespace='monitoring'}",
-                "legendFormat": "{{pod}}",
-                "refId": "A"
-            }
-        ],
-        "gridPos": {"x": 0, "y": 0, "w": 6, "h": 6}
-        }
+#         "targets": [
+#             {
+#                 "expr": "node_cpu_seconds_total{namespace='monitoring'}",
+#                 "legendFormat": "{{pod}}",
+#                 "refId": "A"
+#             }
+#         ],
+#         "gridPos": {"x": 0, "y": 0, "w": 6, "h": 6}
+#         }
 
-mem_panel = {
-        "type": "graph",
-        "title": "Memory Usage",
-        "datasource": "Prometheus",
+# mem_panel = {
+#         "type": "graph",
+#         "title": "Memory Usage",
+#         "datasource": "Prometheus",
 
-        "targets": [
-            {
-                "expr": "node_cpu_seconds_total{namespace='monitoring'}",
-                "legendFormat": "{{pod}}",
-                "refId": "A"
-            }
-        ],
-        "gridPos": {"x": 6, "y": 0, "w": 6, "h": 6}
-        }
-net_panel = {
-        "type": "stat",
-        "title": "Network Usage",
-        "datasource": "Prometheus",
+#         "targets": [
+#             {
+#                 "expr": "node_cpu_seconds_total{namespace='monitoring'}",
+#                 "legendFormat": "{{pod}}",
+#                 "refId": "A"
+#             }
+#         ],
+#         "gridPos": {"x": 6, "y": 0, "w": 6, "h": 6}
+#         }
+# net_panel = {
+#         "type": "stat",
+#         "title": "Network Usage",
+#         "datasource": "Prometheus",
 
-        "targets": [
-            {
-                "expr": "up{namespace='monitoring'}",
-                "legendFormat": "{{pod}}",
-                "refId": "A"
-            }
-        ],
-        "gridPos": {"x": 12, "y": 6, "w": 6, "h": 6}
-        }
+#         "targets": [
+#             {
+#                 "expr": "up{namespace='monitoring'}",
+#                 "legendFormat": "{{pod}}",
+#                 "refId": "A"
+#             }
+#         ],
+#         "gridPos": {"x": 12, "y": 6, "w": 6, "h": 6}
+#         }
 
 dashboard_payload = {
     "dashboard": {
         "id": None,  # Creates a new dashboard
         "uid": "new_dashboard_uid",
         "title": "K8S MLT view Dashboard",
-        "panels": [cpu_panel,
-                   mem_panel,
-                   net_panel],
+        "panels": [pl.cpu_panel("cpu"), pl.mem_panel("mem"), pl.net_panel("net")],
+        "timezone": "browser",
     },
     "overwrite": True
 }
